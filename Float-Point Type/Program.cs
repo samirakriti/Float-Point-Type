03b50e6da476d236d7ea3b4a2c5c1ad4fc8f2afc
samirakriti <45709269+samirakriti@users.noreply.github.com>
2020-07-17 09:33:45 +0300

    Add project files.

---
+++ Float-Point Type/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float_Point_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
